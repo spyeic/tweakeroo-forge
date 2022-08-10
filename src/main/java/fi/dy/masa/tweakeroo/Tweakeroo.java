f5a0a5b189dea7df707847009b9ef8fb0a7d83cd
Eric <lderic0415@gmail.com>
2022-08-10 10:35:32 -0700

    Replace fabric with forge

--- src/main/java/fi/dy/masa/tweakeroo/Tweakeroo.java
+++ src/main/java/fi/dy/masa/tweakeroo/Tweakeroo.java
@@ -1,20 +1,32 @@
 package fi.dy.masa.tweakeroo;
 
+import fi.dy.masa.tweakeroo.gui.GuiConfigs;
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
-import net.fabricmc.api.ModInitializer;
 
-public class Tweakeroo implements ModInitializer
+@Mod(Reference.MOD_ID)
+public class Tweakeroo
 {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
     public static int renderCountItems;
     public static int renderCountXPOrbs;
 
-    @Override
-    public void onInitialize()
-    {
+    public Tweakeroo() {
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+
+        // Register config GUI
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigGuiHandler.ConfigGuiFactory.class,
+                () -> new ConfigGuiHandler.ConfigGuiFactory((minecraftClient, screen) -> {
+                    GuiConfigs gui = new GuiConfigs();
+                    gui.setParent(screen);
+                    return gui;
+                })
+        );
     }
 }
